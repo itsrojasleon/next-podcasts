e3ae632edb5a1ac82c5b4a2aedbb25ae4750b65e
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-07-11 13:49:44 -0500

    styled-jsx

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
 const Home = (props) => {
+  const logo = '/static/platzi-logo.png';
   return (
-    <div>Nope Component</div>
+    <div>
+      <h1>Hello World!</h1>
+      <p>Next Course</p>
+      <img src={logo} alt={`Platzi`} />
+      <style jsx>{`
+        h1 {
+          color: red;
+        }
+        p {
+          color: yellow;
+        }
+        img {
+          max-width: 50%;
+          display: block;
+          margin: 0 auto;
+        }
+      `}</style>
+    </div>
   )
 }
 
