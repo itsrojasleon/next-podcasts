1a6af2fb96873b7b3e3c6d4f0735e7f437dabb1e
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-08-10 14:57:47 -0500

    error 200, 404, 503

--- pages/channel.js
+++ pages/channel.js
@@ -6,24 +6,47 @@ import PodcastList from '../components/podcast-list';
 import ChannelGrid from '../components/channel-grid';
 
 class Channel extends Component {
-  static async getInitialProps({ query }) {
-    const idChannel = query.id;
-    const URL = `https://api.audioboom.com/channels`;
+  static async getInitialProps({ query, res }) {
+    try {
+      const idChannel = query.id;
+      const URL = `https://api.audioboom.com/channels`;
+      const [reqChannel, reqSeries, reqAudios] = await Promise.all([
+        fetch(`${URL}/${idChannel}`),
+        fetch(`${URL}?id=${idChannel}/child_channels`),
+        fetch(`${URL}/${idChannel}/audio_clips`),
+      ]);
 
-    const [reqChannel, reqSeries, reqAudios] = await Promise.all([
-      fetch(`${URL}/${idChannel}`),
-      fetch(`${URL}?id=${idChannel}/child_channels`),
-      fetch(`${URL}/${idChannel}/audio_clips`),
-    ]);
+      if (reqChannel.status >= 400) {
+        res.statusCode = reqChannel.statusCode;
+        return {
+          channel: null,
+          audio_clips: null,
+          channels: null,
+          statusCode: reqChannel.status,
+        }
+      }
 
-    const { body: { channel } } = await reqChannel.json();
-    const { body: { channels } } = await reqSeries.json();
-    const { body: { audio_clips } } = await reqAudios.json(); 
-
-    return { channel, audioClips: audio_clips, channels }
+      const { body: { channel } } = await reqChannel.json();
+      const { body: { channels } } = await reqSeries.json();
+      const { body: { audio_clips } } = await reqAudios.json(); 
+      return { channel, audioClips: audio_clips, channels, statusCode: 200 }
+    }catch(error) {
+      res.statusCode = 503;
+      return {
+        channel: null,
+        audio_clips: null,
+        channels: null,
+        statusCode: 503,
+      }
+    }
   }
   render() {
-    const { channel, audioClips, channels } = this.props;
+    const { channel, audioClips, channels, statusCode } = this.props;
+    if (statusCode !== 200) {
+      return (
+        <Error statusCode={statusCode} />
+      )
+    }
     return (
       <Layout title={channel.title}>
         <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,31 @@ import ChannelGrid from '../components/channel-grid';
 // ¡Doesn't work with getInitialProps.
 
 class Home extends Component {
-  static async getInitialProps() {
-    const URL = 'https://api.audioboom.com/channels/recommended';
-    const request = await fetch(`${URL}`);
-    const { body } = await request.json();
-
-    return {
-      channels: body,
+  static async getInitialProps({ res }) {
+    try {
+      const URL = 'https://api.audioboom.com/channels/recommended';
+      const request = await fetch(`${URL}`);
+      const { body } = await request.json();
+      return {
+        channels: body,
+        statusCode: 200,
+      }
+    }catch(error) {
+      res.statusCode = 503;
+      return {
+        channels: null,
+        statusCode: 503,
+      }
     }
   }
   render() {
     const logo = '/static/platzi-logo.png';
-    const { channels } = this.props;
+    const { channels, statusCode } = this.props;
+    if (statusCode !== 200) {
+      return (
+        <Error statusCode={statusCode} />
+      )
+    }
     return (
       <Layout title="Podcasts">
         <ChannelGrid channels={channels} />
